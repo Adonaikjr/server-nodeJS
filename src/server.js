78ddf5ce9c6d04851cd12b8ab5603da73d92ad1a
adonaikjr <adonaikjr@gmail.com>
2022-06-24 16:21:22 -0300

    add query params

--- src/server.js
+++ src/server.js
@@ -9,6 +9,16 @@ app.get('/message/:id/:user', ( request, response) => {
     `);
 });
 
+app.get('/users', (request, response)=>{
+    const { page, limit } = request.query;
+
+    response.send(`
+    PAGE:${page}.
+    LIMIT:${limit}
+    `)
+})
+
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`server online ${PORT}`) );
