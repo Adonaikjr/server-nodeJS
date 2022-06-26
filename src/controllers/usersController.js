6aba8e1e6f2df36065526e7c3b18cf989d2b8c77
adonaikjr <adonaikjr@gmail.com>
2022-06-26 12:30:26 -0300

    create controllerr

---
+++ src/controllers/usersController.js
@@ -0,0 +1,14 @@
+class usersController {
+    /**
+     * 5 metodos no maximo para cada controller.
+     * 
+     * index - GET para listar varios registros.
+     * show - GET para exibir um registro especifico.
+     * create - POST para criar um registro.
+     * update - PUT para atualizar um registro.
+     * delete - DELETE para remover um registro.
+     * 
+     */
+}
+
+module.exports = usersController
